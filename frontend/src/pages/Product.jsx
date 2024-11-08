c1d25baaee8ad3856eb2e18f07c18c07ce7a0dbd
Hamza Younas <deadbody009@gmail.com>
2024-11-08 10:55:11 +0500

    collection + products

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,3 +1,16 @@
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useParams } from "react-router-dom";
 export default function Product() {
-	return <div>Product</div>;
+	const { productID } = useParams();
+	const { products } = useContext(ShopContext);
+
+	const [productData, setProductData] = useState(false);
+
+	const fetchProductData = async () => {};
+	useEffect(() => {
+		fetchProductData();
+	}, [productID, products]);
+
+	return <div>Products</div>;
 }
